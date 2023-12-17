95b9a990480c9b28f089b7969fe4109eaa0e2609
jorgeortiz41 <jorge.ortiz41@upr.edu>
2023-12-17 05:48:05 -0400

    landing page

---
+++ storycraftx/app/layout.jsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+import { Cairo } from 'next/font/google'
+
+const cairo = Cairo({
+  display: 'swap',
+  weights: [400, 700],
+  styles: ['normal', 'italic'],
+  subsets: ['latin-ext']
+})
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={cairo.className}>
+      <body className="text-white">{children}</body>
+    </html>
+  )
+}
