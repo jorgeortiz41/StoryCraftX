26091bc9242e906e74810d8d49faeb76b623c780
jorgeortiz41 <jorge.ortiz41@upr.edu>
2023-12-17 10:21:39 -0400

    openai api working

---
+++ storycraftx/app/api/completion/route.js
@@ -0,0 +1,29 @@
+import { OpenAIStream, StreamingTextResponse } from "ai"
+import { Configuration, OpenAIApi } from "openai-edge"
+
+export const runtime = "edge"
+
+const configuration = new Configuration({
+  apiKey: "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"
+})
+
+const openai = new OpenAIApi(configuration)
+
+export async function POST(req) {
+  console.log('running post')
+  const json = await req.json()
+  const prompt = [json]
+
+  const res = await openai.createChatCompletion({
+    model: "gpt-4-1106-preview", // "gpt-4-preview"
+    messages: prompt,
+    temperature: 1,
+    top_p: 1,
+    stream: true,
+    max_tokens: 1000,
+  })
+
+  const stream = OpenAIStream(res)
+
+  return new StreamingTextResponse(stream)
+}
