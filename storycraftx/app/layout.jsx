4bd262a39b0efc13255840588f9c36b4da296fe8
jorgeortiz41 <jorge.ortiz41@upr.edu>
2023-12-18 12:22:16 -0400

    centered flexbox for all pages on layout

--- storycraftx/app/layout.jsx
+++ storycraftx/app/layout.jsx
@@ -13,7 +13,7 @@ const cairo = Cairo({
 export default function RootLayout({ children }) {
   return (
       <html lang="en" className={cairo.className}>
-        <body className="text-white bg-gradient-to-r from-purple-600 to-indigo-600">
+        <body className="text-white bg-gradient-to-r from-purple-600 to-indigo-600 flex justify-center items-center p-24">
           {children}       
         </body>
       </html>  
